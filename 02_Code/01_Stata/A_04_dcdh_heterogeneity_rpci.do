c71e9087909167c56a82d0a65b0b1105a41616fe
Marco Alejandro Medina <mamedinasalgado@gmail.com>
2023-03-27 23:56:08 -0600

    Heterogeneity figures

---
+++ 02_Code/01_Stata/A_04_dcdh_heterogeneity_rpci.do
@@ -0,0 +1,197 @@
+/*******************************************************************************
+@Name: dcdh_heterogeneity_rpci.do
+
+@Author: Marco Medina
+
+@Date: 02/03/2023
+
+@In: panel_rpci.dta
+	 
+@Out: 
+*******************************************************************************/
+
+********************
+version 17.0
+clear all
+cd "$directory"
+********************
+
+* Use panel_rpci.dta
+use "01_Data/03_Working/panel_rpci.dta", clear
+
+* Gen heterogeneity variables
+gen hombre = [sexo == 0]
+gen mujer = [sexo == 1]
+
+gen age_15_25 = [inlist(base_rango, 1, 2, 3)]
+gen age_25_35 = [inlist(base_rango, 4, 5)]
+gen age_35_45 = [inlist(base_rango, 6, 7)]
+gen age_45_55 = [inlist(base_rango, 8, 9)]
+gen age_55_65 = [inlist(base_rango, 10, 11)]
+gen age_65 = [inlist(base_rango, 12, 13, 14)]
+
+gen sal_min_1_2 = [inlist(base_num_sal_min, 1)]
+gen sal_min_2_3 = [inlist(base_num_sal_min, 2)]
+gen sal_min_3_5 = [inlist(base_num_sal_min, 3, 4)]
+gen sal_min_5 = [base_num_sal_min >= 5]
+
+gen frontera = [base_frontera == 1]
+gen no_frontera = [base_frontera == 0]
+
+gen reg_centro = [inlist(base_cve_ent_final, 9, 12, 13, 15, 17, 20, 21, 29)]
+gen reg_centro_occ = [inlist(base_cve_ent_final, 1, 6, 11, 14, 16, 18, 22, 24, 32)]
+gen reg_norte = [inlist(base_cve_ent_final, 2, 3, 5, 8, 10, 19, 25, 26, 28)]
+gen reg_sur = [inlist(base_cve_ent_final, 4, 7, 23, 27, 30, 31)]
+
+gen ind_agricul = [base_div_final == 0]
+gen ind_transf = [base_div_final == 3]
+gen ind_constr = [base_div_final == 4]
+gen ind_commerce = [base_div_final == 6]
+gen ind_transport = [base_div_final == 7]
+gen ind_services = [base_div_final == 8 | base_div_final == 9]
+
+gen size_1 = [base_size_cierre == 1]
+gen size_2 = [base_size_cierre == 2]
+gen size_6 = [base_size_cierre == 3]
+gen size_51 = [base_size_cierre == 4]
+gen size_251 = [base_size_cierre == 5]
+gen size_501 = [base_size_cierre == 6]
+gen size_1001 = [base_size_cierre == 7]
+
+* Gen labels
+label var hombre "Men"
+label var mujer "Women"
+label var base_outsourcing "Outsourcing"
+label var base_te "Eventual"
+
+label var age_15_25 "15 to 25 years old"
+label var age_25_35 "25 to 35 years old"
+label var age_35_45 "35 to 45 years old"
+label var age_45_55 "45 to 55 years old"
+label var age_55_65 "55 to 65 years old"
+label var age_65 "65+ years old"
+
+label var sal_min_1_2 "1 to 2 minimum wages"
+label var sal_min_2_3 "2 to 3 minimum wages"
+label var sal_min_3_5 "3 to 5 minimum wages"
+label var sal_min_5 "More than 5 minimum wages"
+
+label var frontera "MX-USA Border"
+label var no_frontera "Away from MX-USA Border"
+
+label var reg_centro "Central"
+label var reg_centro_occ "Central-West"
+label var reg_norte "North"
+label var reg_sur "South-East"
+
+label var ind_agricul "Agriculture"
+label var ind_transf "Transformation"
+label var ind_constr "Construction"
+label var ind_commerce "Commerce"
+label var ind_transport "Transportation & Communication"
+label var ind_services "Services"
+
+label var size_1 "1 worker"
+label var size_2 "2-5 workers"
+label var size_6 "6-50 workers"
+label var size_51 "51-250 workers"
+label var size_251 "251-500 workers"
+label var size_501 "501-1000 workers"
+label var size_1001 "1000+ workers"
+
+* Define variables
+local vars alta sal_formal sal_cierre log_sal_cierre cambio_cierre sal_diff
+local hetero_vars hombre mujer base_outsourcing base_te ///
+				  age_15_25 age_25_35 age_35_45 age_45_55 age_55_65 age_65 ///
+				  sal_min_1_2 sal_min_2_3 sal_min_3_5 sal_min_5 ///
+				  frontera no_frontera ///
+				  reg_centro reg_centro_occ reg_norte reg_sur ///
+				  ind_agricul ind_transf ind_constr ind_commerce ind_transport ind_services ///
+				  size_1 size_2 size_6 size_51 size_251 size_501 size_1001
+
+********************************************
+* De Chaisemartin & d'Haultfoeuille (2020) *
+********************************************
+
+foreach dep_var in `vars' {
+	
+	foreach het_var in `hetero_vars' {
+	
+	did_multiplegt `dep_var' download_monthly periodo_monthly rpci_vig if `het_var', ///
+				   first robust_dynamic dynamic(12) placebo(24) breps(250) cluster(idnss) seed(541314)
+				   
+	mat `dep_var'_b_dcdh  = nullmat(`dep_var'_b_dcdh), e(effect_average)
+	mat `dep_var'_se_dcdh = nullmat(`dep_var'_se_dcdh), e(se_effect_average)
+	
+	
+	* Event study	   
+	event_plot mat1_dcdh#mat2_dcdh, stub_lag(Effect_#) stub_lead(Placebo_#) ///
+	       together plottype("scatter") trimlead(12) trimlag(12) ///
+		   graph_opt(xline(-0.5, lcolor(gs8) lpattern(dash)) yline(0, lcolor(gs8)) ///
+		   graphregion(color(white)) xlabel(-12(2)12) xsize(7.5) ///
+		   xtitle("Months since registering for the RPCI") ytitle("Average effect") ///
+		   title("")) ///
+		   lag_opt1(msymbol(O) color("0 69 134")) lag_ci_opt1(color("0 69 134"))
+
+	graph export "04_Figures/$muestra/event_study_`depvar'_`het_var'_dcdh.pdf", replace
+	
+	* Connected event study	   
+	event_plot mat1_dcdh#mat2_dcdh, stub_lag(Effect_#) stub_lead(Placebo_#) ///
+		   together trimlead(12) trimlag(12) ///
+		   graph_opt(xline(-0.5, lcolor(gs8) lpattern(dash)) yline(0, lcolor(gs8)) ///
+		   graphregion(color(white)) xlabel(-12(2)12) xsize(7.5) ///
+		   xtitle("Months since registering for the RPCI") ytitle("Average effect") ///
+		   title("")) ///
+		   lag_opt1(msymbol(O) color("0 69 134")) lag_ci_opt1(color("0 69 134 %45"))
+
+	graph export "04_Figures/$muestra/event_study_`depvar'_`het_var'_dcdh_connected.pdf", replace
+	
+	* Connected event study	- paper
+	event_plot mat1_dcdh#mat2_dcdh, stub_lag(Effect_#) stub_lead(Placebo_#) ///
+		   together trimlead(24) trimlag(12) ///
+		   graph_opt(xline(-0.5, lcolor(gs8) lpattern(dash)) yline(0, lcolor(gs8)) ///
+		   graphregion(color(white)) xlabel(-24(2)12) xsize(7.5) ///
+		   xtitle("Months since registering for the RPCI") ytitle("Average effect") ///
+		   title("")) ///
+		   lag_opt1(msymbol(O) color("0 69 134")) lag_ci_opt1(color("0 69 134 %45"))
+
+	graph export "04_Figures/$muestra/event_study_`depvar'_`het_var'_dcdh_connected_paper.pdf", replace
+	
+	}
+	
+	mat `dep_var'_b_dcdh_t = `dep_var'_b_dcdh'
+	mat `dep_var'_se_dcdh_t = `dep_var'_se_dcdh'
+	
+	mat b_dcdh = nullmat(b_dcdh), `dep_var'_b_dcdh_t
+	mat se_dcdh = nullmat(se_dcdh), `dep_var'_se_dcdh_t
+}
+
+foreach mat in b_dcdh se_dcdh {
+	
+	* Add column and row names
+	mat coln `mat' = `vars'
+	mat rown `mat' = `hetero_vars'
+	
+	* Make the matrix a dataset
+	clear
+	svmat `mat', names(col)
+	
+	* Create a column with the matrix rownames
+	local rownames: rownames `mat'
+	di "`rownames'"
+	local rows = rowsof(`mat')
+	di `rows'
+	gen hetero_var = ""
+
+	forvalues i = 1 / `rows' {
+		local rowname = word("`rownames'", `i')
+		replace hetero_var = "`rowname'" if _n == `i'
+	}
+
+	* Export the dataset
+	export delimited using "01_Data/04_Temp/`mat'_heterogeneity.csv", replace delim("|")
+}
+
+*matrix list b_dcdh
+*matrix list se_dcdh
+
---
+++ 02_Code/02_R/B_04_heterogeneity_rpci.R
@@ -0,0 +1,215 @@
+#' @Nombre: heterogeneity_rpci.R
+#' 
+#' @Author: Marco Medina
+#' 
+#' @Descripción: Create graphs about heterogeneity in the RPCI effect.
+#' 
+#' @In: b_dcdh_heterogeneity.csv (exported from A_04_dcdh_heterogeneity_rpci.do)
+#'      se_dcdh_heterogeneity.csv (exported from A_04_dcdh_heterogeneity_rpci.do)
+#' 
+#' @Out: 
+#' 
+
+# Libraries
+pacman::p_load(readr, dplyr, ggplot2, ggthemes, scales, tidyr, stringr)
+
+# Load data
+b <- read_delim("01_Data/04_Temp/b_dcdh_heterogeneity.csv", delim = "|") %>%
+  select(-cambio_cierre, -sal_diff) %>%
+  pivot_longer(cols = c(alta, sal_formal, sal_cierre, log_sal_cierre),
+               names_to = "var", 
+               values_to = "b")
+
+se <- read_delim("01_Data/04_Temp/se_dcdh_heterogeneity.csv", delim = "|") %>%
+  select(-cambio_cierre, -sal_diff) %>%
+  pivot_longer(cols = c(alta, sal_formal, sal_cierre, log_sal_cierre),
+               names_to = "var", 
+               values_to = "se")
+
+# Create database with confidence intervals and significance
+data <- b %>% 
+  left_join(se) %>%
+  mutate(p_value = 2*(1-pnorm(abs(b/se)))) %>%
+  mutate(signif = ifelse(p_value <= 0.01,
+                         "***",
+                         ifelse(p_value <= 0.05,
+                                "**",
+                                ifelse(p_value <= 0.1,
+                                       "*",
+                                       "")))) %>%
+  mutate(upper_ci = b + 1.96*se,
+         lower_ci = b - 1.96*se) %>%
+  mutate(type = ifelse(str_detect(hetero_var, pattern = "^size"),
+                       7,
+                       ifelse(str_detect(hetero_var, pattern = "^ind"),
+                              6,
+                              ifelse(str_detect(hetero_var, pattern = "^reg"),
+                                     5,
+                                     ifelse(str_detect(hetero_var, pattern = "frontera"),
+                                            4,
+                                            ifelse(str_detect(hetero_var, pattern = "^sal_min"),
+                                                   3,
+                                                   ifelse(str_detect(hetero_var, pattern = "^age"),
+                                                          2,
+                                                          1)))))))
+
+# Factor variables
+data$hetero_var <- factor(data$hetero_var,
+                          levels = c("size_1001",
+                                     "size_501",
+                                     "size_251",
+                                     "size_51",
+                                     "size_6",
+                                     "size_2",
+                                     "size_1",
+                                     "ind_services",
+                                     "ind_transport",
+                                     "ind_commerce",
+                                     "ind_constr",
+                                     "ind_transf",
+                                     "ind_agricul",
+                                     "reg_sur",
+                                     "reg_norte",
+                                     "reg_centro_occ",
+                                     "reg_centro",
+                                     "no_frontera",
+                                     "frontera",
+                                     "sal_min_5",
+                                     "sal_min_3_5",
+                                     "sal_min_2_3",
+                                     "sal_min_1_2",
+                                     "age_65",
+                                     "age_55_65",
+                                     "age_45_55",
+                                     "age_35_45",
+                                     "age_25_35",
+                                     "age_15_25",
+                                     "base_te",
+                                     "base_outsourcing",
+                                     "mujer",
+                                     "hombre"),
+                          labels = c("1000+ workers",
+                                     "501-1000 workers",
+                                     "251-500 workers",
+                                     "51-250 workers",
+                                     "6-50 workers",
+                                     "2-5 workers",
+                                     "1 workers",
+                                     "Services",
+                                     "Transportation & Comunication",
+                                     "Commerce",
+                                     "Construction",
+                                     "Transformation",
+                                     "Agriculture",
+                                     "South-East",
+                                     "North",
+                                     "Central-West",
+                                     "Central",
+                                     "Away from MX-USA Border",
+                                     "MX-USA Border",
+                                     "More than 5 minimum wages",
+                                     "3 to 5 minumum wages",
+                                     "2 to 3 minumum wages",
+                                     "1 to 2 minumum wages",
+                                     "65+ years old",
+                                     "55 to 65 years old",
+                                     "45 to 55 years old",
+                                     "35 to 45 years old",
+                                     "25 to 35 years old",
+                                     "15 to 25 years old",
+                                     "Eventual",
+                                     "Outsourcing",
+                                     "Woman",
+                                     "Man"))
+
+data$var <- factor(data$var,
+                   levels = c("alta",
+                              "sal_formal",
+                              "sal_cierre",
+                              "log_sal_cierre"),
+                   labels = c("Enrolled",
+                              "Formal Wage",
+                              "Wage",
+                              "Log Wage"))
+
+data$type <- factor(data$type,
+                    levels = c(1:7),
+                    labels = c("Worker characteristics",
+                               "Worker age",
+                               "Worker wage in min. wages",
+                               "Job Location: MX-US Border",
+                               "Job Location: Regions",
+                               "Firm Industry",
+                               "Firm Size"))
+
+  
+
+## Graphs ----------------------------------------------------------------------
+
+# Worker Characteristics
+ggplot(data %>% filter(hetero_var != "65+ years old", type %in% c("Worker characteristics",
+                                                                  "Worker age",
+                                                                  "Worker wage in min. wages")),
+       aes(x = hetero_var, y = b, color = type, alpha = signif)) +
+  facet_wrap(~var,
+             ncol = 4,
+             scales = "free_x") + 
+  geom_point(aes(shape = type), size = 2) +
+  geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci, size = signif), width = 0.3) +
+  geom_hline(yintercept = 0, color = "black", alpha = 0.75, lty = 2) +
+  scale_color_manual(name = "Heterogeneity type",
+                     values = c("#004586", "#ff420e", "#579d1c")) +
+  scale_shape_manual(name = "Heterogeneity type",
+                     values = c(16, 15, 17)) +
+  scale_size_discrete(name = "Coefficient significance",
+                      range = c(0.6, 0.9)) +
+  scale_alpha_discrete(name = "Coefficient significance",
+                       range = c(0.5, 1)) +
+  guides(color = guide_legend(order = 1),
+         shape = guide_legend(order = 1),
+         size = guide_legend(order = 2),
+         alpha = guide_legend(order = 2)) +
+  coord_flip() +
+  labs(x = "Heterogeneity variable",
+       y = "Average effect") +
+  theme_calc()
+
+ggsave("04_Figures/muestra_10porciento/dcdh_heterogeneity_worker_characteristics.pdf",
+       width = 12,
+       height = 5)
+
+
+# Firm Characteristics
+ggplot(data %>% filter(hetero_var != "65+ years old", !(type %in% c("Worker characteristics",
+                                                                    "Worker age",
+                                                                    "Worker wage in min. wages"))),
+       aes(x = hetero_var, y = b, color = type, alpha = signif)) +
+  facet_wrap(~var,
+             ncol = 4,
+             scales = "free_x") + 
+  geom_point(aes(shape = type), size = 2) +
+  geom_errorbar(aes(ymin = lower_ci, ymax = upper_ci, size = signif), width = 0.3) +
+  geom_hline(yintercept = 0, color = "black", alpha = 0.75, lty = 2) +
+  scale_color_manual(name = "Heterogeneity type",
+                     values = c("#004586", "#ff420e", "#579d1c", "#ff950e")) +
+  scale_shape_manual(name = "Heterogeneity type",
+                     values = c(16, 15, 17, 18)) +
+  scale_size_discrete(name = "Coefficient significance",
+                      range = c(0.6, 0.9)) +
+  scale_alpha_discrete(name = "Coefficient significance",
+                       range = c(0.5, 1)) +
+  guides(color = guide_legend(order = 1),
+         shape = guide_legend(order = 1),
+         size = guide_legend(order = 2),
+         alpha = guide_legend(order = 2)) +
+  coord_flip() +
+  labs(x = "Heterogeneity variable",
+       y = "Average effect") +
+  theme_calc()
+
+ggsave("04_Figures/muestra_10porciento/dcdh_heterogeneity_firm_characteristics.pdf",
+       width = 12,
+       height = 7)
+
+
+         
